2552d1647af8df571f1be05088ef4e6cdc70f71b
Edson Sousa <edson3711.es@gmail.com>
2023-02-26 19:49:44 -0300

    commit 1

---
+++ src/controller/UnidadeController.java
@@ -0,0 +1,108 @@
+package controller;
+
+import java.util.ArrayList;
+import javax.swing.JTable;
+import javax.swing.ListSelectionModel;
+import javax.swing.RowSorter;
+import javax.swing.table.TableModel;
+import javax.swing.table.TableRowSorter;
+import model.ModeloTabela;
+import model.Unidade;
+import service.UnidadeService;
+import view.UnidadeForm;
+
+public class UnidadeController {
+
+//    Conversor conversor;
+    private final UnidadeForm view;
+
+//    private Unidade beans = new Unidade();
+    UnidadeService service = new UnidadeService();
+
+    public UnidadeController(UnidadeForm view) {
+        this.view = view;
+    }
+
+    public void clear() {
+        view.getTxtSigla().setText(null);
+        view.getTxtDescricao().setText(null);
+        view.getTxtPesquisa().setText(null);
+        view.getCkFragmentado().setSelected(false);
+        view.getTxtSigla().requestFocus();
+        view.getBtnSalvar().setEnabled(false);
+        view.getBtnSalvar().setText("Salvar");
+        view.setIdUnidade(0);
+        view.getBtnExcluir().setVisible(false);
+        view.getTxtDataHora().setText(" ");
+        view.getTxtNomeUsuario().setText(" ");
+        loadTable("", false);
+
+    }
+
+    public void create() throws Exception {
+        Unidade u = service.create(view);
+        if (u.getId() >= 1) {
+            clear();
+            u.setId(null);
+        }
+    }
+
+    public void destroy(int id) {
+//        beans.setId(id);
+//        beans.setUsuario(1);
+//        if (delete(beans) == true) {
+//            clear();
+//        } else {
+//            deactivate(beans);
+//            clear();
+//        }
+
+    }
+
+    public void loadTable(String texto, boolean verdade) {
+        String[] colunas = new String[]{"Id", "Sigla", "Descrição", "Fragmentado"};
+        ArrayList dados;
+
+        dados = (ArrayList) service.consultaTodos();
+
+        ModeloTabela modelo = new ModeloTabela(dados, colunas);
+        view.getTabela().setModel(modelo);
+        RowSorter<TableModel> sorter = new TableRowSorter<>(modelo);
+        view.getTabela().setRowSorter(sorter);
+//         view.getTabela().getColumnModel().getColumn(0).setPreferredWidth(60);
+        view.getTabela().getColumnModel().getColumn(0).setResizable(true);
+//         view.getTabela().getColumnModel().getColumn(1).setPreferredWidth(60);
+        view.getTabela().getColumnModel().getColumn(1).setResizable(true);
+//         view.getTabela().getColumnModel().getColumn(2).setPreferredWidth(60);
+        view.getTabela().getColumnModel().getColumn(2).setResizable(true);
+        view.getTabela().getTableHeader().setReorderingAllowed(false);
+        view.getTabela().getColumnModel().getColumn(3).setResizable(true);
+        view.getTabela().getTableHeader().setReorderingAllowed(false);
+        view.getTabela().setAutoResizeMode(JTable.AUTO_RESIZE_ALL_COLUMNS);
+        view.getTabela().setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+
+    }
+
+    public void pressKeys() {
+        if (view.getTxtSigla().getText().length() >= 2 && view.getTxtDescricao().getText().length() > 2) {
+            view.getBtnSalvar().setEnabled(true);
+        } else {
+            view.getBtnSalvar().setEnabled(false);
+        }
+    }
+
+    public void loadRecord(int id) {
+//        for (UnidadeBeans u : findOne(id)) {
+//            view.setIdUnidade(id);
+//            view.getTxtSigla().setText(u.getSigla());
+//            view.getTxtDescricao().setText(u.getDescricao());
+//            view.getCkFragmentado().setSelected(u.isFragmentado());
+//
+//            view.getTxtDataHora().setText("" + conversor.DataAtual(u.getDataHora()));
+//            view.getTxtNomeUsuario().setText("teste");
+//        }
+//        pressKeys();
+//        view.getBtnSalvar().setText("Atualizar");
+    }
+
+}
---
+++ src/model/Usuario.java
@@ -0,0 +1,78 @@
+package model;
+
+import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@Entity
+public class Usuario implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String login;
+    private String senha;
+    private Boolean ativo;
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "data_hora")
+    private Date dataHora;
+
+    public Usuario() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public Boolean getAtivo() {
+        return ativo;
+    }
+
+    public void setAtivo(Boolean ativo) {
+        this.ativo = ativo;
+    }
+
+    public Date getDataHora() {
+        return dataHora;
+    }
+
+    public void setDataHora(Date dataHora) {
+        this.dataHora = dataHora;
+    }
+
+}
